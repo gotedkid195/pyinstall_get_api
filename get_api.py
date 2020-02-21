9186129a76df0338ed88272aedfb0d93887ea728
gotedkid195 <nguyenhuucanh95@gmail.com>
2020-02-21 15:31:43 +0700

    run thread

--- get_api.py
+++ get_api.py
@@ -7,42 +7,32 @@ import errno
 
 
 class GetAPI(threading.Thread):
-    def __init__(self, url, token, *args, **kwargs):
+    def __init__(self, url, token, freq, direct,  *args, **kwargs):
         super(GetAPI, self).__init__(*args, **kwargs)
         self.url = url
         self.token = token
-        self.stop_request = threading.Event()
+        self.freq = freq
+        self.direct = direct
+        self._stop_event = threading.Event()
 
     def stop(self):
-        self.stop_request.set()
+        print("current_thread",threading.current_thread())
+        print("activeCount",threading.activeCount())
+        print("enumerate",threading.enumerate())
+
+        self._stop_event.set()
+        self.join(1)
+
+        print("stop thread", self)
+        print("current_thread",threading.current_thread())
+        print("activeCount",threading.activeCount())
+        print("enumerate",threading.enumerate())
 
     def run(self) -> None:
-        while True:
+        for i in range(100):
             response = requests.get(self.url, headers={"Authorization": f"{self.token}"})
-            print(f"Get api {response}")
-            # filename = '/abc/'
-            # os.makedirs(filename)
-            # dir_path = os.path.dirname(os.path.realpath(__file__))
-            # print(dir_path)
-            # os.chdir(dir_path)
-            # os.makedirs('abc/', exist_ok=True)
-            # access_rights = 0o755
-            # os.mkdir('abc', access_rights)
-
-            #
-            # folder = 'abc1'
-            # if not os.path.exists(folder):
-            #     os.mkdir(folder)
-            # elif not os.path.isdir(folder):
-            #     return
-            # f = open(os.path.join('abc1/data.json'), 'wb')
-
-
-            f = open(os.path.join('/home/canhnguyen/part_manager/data.json'), 'wb')
+            print(f"{i} Working: {response}")
+            f = open(os.path.join(f'{self.direct}/data.json'), 'wb')
             f.write(response.content)
             f.close()
-
-
-            # with open('/abc/data.json', 'wb') as f:
-            #     f.write(response.content)
             time.sleep(1)
--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import os
 import sys
-if sys.version_info.major == 3:
-    import tkinter as tk
-    from tkinter import ttk, font
-else:
-    import Tkinter as tk
-    import ttk
+import tkinter as tk
+from tkinter import ttk, font
 from ttkthemes import ThemedTk
 from db import Database
 from tkinter import messagebox, filedialog
 from get_api import GetAPI
-
+from tkinter import *
 
 db = Database('store.db')
 
-from tkinter import *
 
 class Example(ThemedTk):
-    """
-    Example that is used to create screenshots for new themes.
-    """
     def __init__(self):
         ThemedTk.__init__(self, themebg=True)
         self.url_label = ttk.Label(self, text="Enter request URL")
@@ -30,7 +22,7 @@ class Example(ThemedTk):
 
         self.frq_label = ttk.Label(self, text="Frequency of taking data")
 
-        baudrates = (
+        freq = (
             9600,
             19200,
             38400,
@@ -41,15 +33,15 @@ class Example(ThemedTk):
             500000,
             512000,
         )
-        self.baudrate = tk.StringVar(self)
-        self.frq_option = ttk.OptionMenu(self, self.baudrate, *baudrates)
+        self.frequency = tk.StringVar(self)
+        self.frq_option = ttk.OptionMenu(self, self.frequency, *freq)
 
-        self.abc = ttk.Label(self, text="Bản quyền thuộc về Teslateq Co., Ltd.")
+
         self.parts_list = tk.Listbox(self, height=8, width=60, border=1, background='white')
 
-        self.sto_label = ttk.Label(self, text="Data storage place")
-        self.sto_entry = ttk.Entry(self, width=30, textvariable=tk.StringVar())
-        self.sto_button = ttk.Button(self, text="Select the storage directory", command=self.file_dialog)
+        self.direct = tk.StringVar()
+        self.sto_label = tk.Label(self, textvariable=self.direct)
+        self.sto_button = ttk.Button(self, text="Please select the path to direct the returned data", command=self.file_dialog)
 
         self.save_button = ttk.Button(self, text="Save", command=self.add_item)
 
@@ -57,6 +49,7 @@ class Example(ThemedTk):
 
         self.stop_button = ttk.Button(self, text="Stop", command=self.stop)
 
+        self.license = ttk.Label(self, text="Bản quyền thuộc về Teslateq Co., Ltd.")
         self.grid_widgets()
         self.get_list()
         self.thread = None
@@ -67,49 +60,43 @@ class Example(ThemedTk):
         sticky = {"sticky": "nswe"}
         self.url_label.grid(row=0, column=0, padx=20, pady=20, **sticky)
         self.url_entry.grid(row=0, column=1, sticky='we')
-        # self.abc.grid()
+
         self.token_label.grid(row=1, column=0, padx=20, pady=20, **sticky)
         self.token_entry.grid(row=1, column=1, sticky='we')
 
         self.frq_label.grid(row=2, column=0, padx=20, pady=20, **sticky)
         self.frq_option.grid(row=2, column=1, sticky='we')
 
-        self.sto_label.grid(row=3, column=0, padx=20, pady=20, **sticky)
-        self.sto_entry.grid(row=3, column=1, sticky='w')
-        self.sto_button.grid(row=3, column=1, sticky='e')
+        self.sto_button.grid(row=3, column=0, columnspan=3, padx=20, pady=15, sticky='w')
+        self.sto_label.grid(row=4, column=0, padx=20, columnspan=3, sticky='w')
 
+        self.start_button.grid(row=5, column=1, pady=15, sticky='w')
+        self.stop_button.grid(row=5, column=1)
+        self.save_button.grid(row=5, column=1, sticky='e')
 
-        self.start_button.grid(row=4, column=1, sticky='w')
-        self.stop_button.grid(row=4, column=1)
-        self.save_button.grid(row=4, column=1, sticky='e')
-
-        self.parts_list.grid(row=5, column=0, columnspan=2, rowspan=2, pady=20)
+        self.parts_list.grid(row=6, column=0, columnspan=2, rowspan=2, pady=20)
         self.parts_list.bind('<<ListboxSelect>>', self.select_item)
+
+        self.license.grid(row=7, column=0, columnspan=3, sticky='s')
 
     def file_dialog(self):
         filename = filedialog.askdirectory()
-        self.sto_entry.delete(0, END)
-        self.sto_entry.insert(END, filename)
-        print(filename)
+        self.direct.set(filename)
 
     def get_list(self):
         self.parts_list.delete(0, tk.END)
         self.parts_list.insert(tk.END, db.last())
-        a = db.lasts()
-        print(a)
-
 
     def add_item(self):
         if self.url_entry.get() == '' or self.token_entry.get() == '':
             messagebox.showerror("Required Fields", " Please fill in all fields.")
             return
         # Insert into DB
-        print(self.url_entry.get(), self.token_entry.get(), self.baudrate.get(), self.sto_entry.get())
-        db.insert(self.url_entry.get(), self.token_entry.get(), self.baudrate.get(), self.sto_entry.get())
+        db.insert(self.url_entry.get(), self.token_entry.get(), self.frequency.get(), self.direct.get())
         # Clear list
         self.parts_list.delete(0, tk.END)
         # Insert into list
-        self.parts_list.insert(tk.END, (self.url_entry.get(), self.token_entry.get(), self.baudrate.get(), self.sto_entry.get()))
+        self.parts_list.insert(tk.END, (self.url_entry.get(), self.token_entry.get(), self.frequency.get(), self.direct.get()))
 
         self.get_list()
 
@@ -117,39 +104,34 @@ class Example(ThemedTk):
     def select_item(self, event):
         # # Create global selected item to use in other functions
         # global self.selected_item
-
         try:
-            # Get index
             index = self.parts_list.curselection()[0]
             # Get selected item
             self.selected_item = self.parts_list.get(index)
-            # print(selected_item) # Print tuple
-            print(index, self.selected_item)
             # Add text to entries
             self.url_entry.delete(0, tk.END)
             self.url_entry.insert(tk.END, self.selected_item[1])
             self.token_entry.delete(0, tk.END)
             self.token_entry.insert(tk.END, self.selected_item[2])
-            self.baudrate.set(self.selected_item[3])
-            self.sto_entry.delete(0, tk.END)
-            self.sto_entry.insert(tk.END, self.selected_item[4])
-
+            self.frequency.set(self.selected_item[3])
+            self.direct.set(self.selected_item[4])
         except IndexError:
             pass
 
     def start(self):
-        if self.part_text.get() == '' or self.customer_text.get() == '':
+        if self.url_entry.get() == '' or self.token_entry.get() == '':
             messagebox.showerror(
                 "Required Fields", " Please fill in all fields.")
             return
 
         self.thread = GetAPI(
-            url=self.part_text.get(),
-            token=self.customer_text.get(),
+            url=self.url_entry.get(),
+            token=self.token_entry.get(),
+            freq=self.frequency.get(),
+            direct=self.direct.get(),
         )
         self.thread.start()
-        print('Start!')
-        self.populate_list()
+        print("Started", self.thread)
 
     def stop(self):
         if self.thread:
@@ -159,7 +141,7 @@ class Example(ThemedTk):
 
 if __name__ == '__main__':
     example = Example()
-    example.geometry("700x300+300+300")
+    example.geometry("700x350+300+300")
     example.set_theme("breeze")
     example.title('Iotwhynot API Software')
     example.iconphoto(True, PhotoImage(file="image/a.png"))
